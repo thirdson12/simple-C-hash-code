17a1e15ab6be380c798fea49df2157661bc1c8ad
bertu <74742652+thirdson12@users.noreply.github.com>
2023-03-09 15:55:38 +0200

    hash function code
    hash function code

---
+++ hash.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+unsigned int hash_function(const char* str, int m) {
+    unsigned int hash = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        hash = hash * 7 + str[i];
+    }
+    return hash % m;
+}
+
+int main()
+{
+    int m = 10000;
+    const char* str = "hello sdjhaknnhnhnhnhnsgdk";
+    unsigned int hash = hash_function(str, m);
+    printf("Hash value: %d\n", hash);
+    printf("Hash value: %x\n", hash);
+    return 0;
+}
+
